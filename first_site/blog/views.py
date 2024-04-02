a916dfbb9a299cf28f7430694154b9e5196f0e75
Grisha Krim <you@example.com>
2024-04-02 21:48:19 +0300

    1

--- first_site/blog/views.py
+++ first_site/blog/views.py
@@ -1,32 +1,35 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
-posts = [
-    {
-        "title":"Test post 1",
-        "author":"John Dou",
-        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-        "published_at":"October 1 1992"
-    },
-    {
-        "title":"Test post 2",
-        "author":"John Dou",
-        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ",
-        "published_at":"October 1 1992"
-    },
-]
-
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+from django.contrib import messages
 
 
 # Create your views here.
+def create_post(request):
+    if request.method == "GET":
+        context = {'form': PostForm()}
+        return render(request,'blog/post_form.html',context)
+    elif request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Success")
+            return redirect('posts')
+        else:
+            messages.error(request,"Please check form")
+            return render(request,'blog/post_form.html',{'form': form})
+    else:
+        return False
+
 
 def home(request):
+    posts = Post.objects.all()
     context = {
         'posts': posts,
-        'title': "Main Blog page"
     }
-    return render(request,"blog/home.html",context)
+    return render(request, "blog/home.html", context)
 
 
 def about(request):
-    return render(request,"blog/about.html")
+    return render(request, "blog/about.html")
