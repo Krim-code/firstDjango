840abe1e9dc578fa36459fd6168f41e158bddd10
Grisha Krim <you@example.com>
2024-04-13 18:40:01 +0300

    1

--- first_site/blog/views.py
+++ first_site/blog/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostForm
 from django.contrib import messages
@@ -22,6 +22,32 @@ def create_post(request):
     else:
         return False
 
+def edit_post(request,id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == "GET":
+        context = {'form': PostForm(instance=post),"id":id}
+        return render(request,'blog/post_form.html', context)
+
+    elif request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'The post is update')
+            return redirect('posts')
+        else:
+            messages.error(request,"Please check your data")
+            return render(request,'blog/post_form.html', {"form": form})
+
+def delete_post(request,id):
+    post = get_object_or_404(Post, id=id)
+    context = {'post':post}
+    if request.method == "GET":
+        return render(request,'blog/post_confirm_delete.html',context)
+    elif request.method == "POST":
+        post.delete()
+        messages.success(request, "The post has been deleted")
+        return redirect('posts')
 
 def home(request):
     posts = Post.objects.all()
---
+++ first_site/users/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import LoginForm
+
+# Create your views here.
+
+
+def sign_in(request):
+    if request.method == "GET":
+        form = LoginForm()
+        return render(request, 'users/login.html',context={'form':form})
